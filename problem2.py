672a38cb31459f9cfd56fb215f6c1e5a8fdbde65
CeferAzmiu01 <47792097+CeferAzmiu01@users.noreply.github.com>
2019-02-19 22:49:23 +0400

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,208 @@
+#2ci tapshiriq
+while True:
 
+    sesli_reqem_onluq = ["on", "iyirmi", "otuz", "qirx", "elli", "altmish", "yetmish", "seksen", "doxsan","yuz"]
+    sesli_reqem_adi = ["bir", "iki", "uc", "dord", "bes", "alti", "yeddi", "sekkiz", "doqquz"]
+    reqem_daxil = int(input("1'le 100 arasi reqem daxil edin:"))
+    if reqem_daxil > 100:
+        print("100den yuxari reqem daxil etdiniz!")
+    elif reqem_daxil == 1:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[0]))
+    elif reqem_daxil == 2:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[1]))
+    elif reqem_daxil == 3:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[2]))
+    elif reqem_daxil == 4:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[3]))
+    elif reqem_daxil == 5:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[4]))
+    elif reqem_daxil == 6:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[5]))
+    elif reqem_daxil == 7:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[6]))
+    elif reqem_daxil == 8:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[7]))
+    elif reqem_daxil == 9:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_adi[8]))
+    elif reqem_daxil == 10:
+        print("daxil etdiyiniz reqem:{}".format(sesli_reqem_onluq[0]))
+    elif reqem_daxil == 11:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[0]))
+    elif reqem_daxil == 12:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[1]))
+    elif reqem_daxil == 13:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[2]))
+    elif reqem_daxil == 14:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[3]))
+    elif reqem_daxil == 15:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[4]))
+    elif reqem_daxil == 16:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[5]))
+    elif reqem_daxil == 17:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[6]))
+    elif reqem_daxil == 18:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[7]))
+    elif reqem_daxil == 19:
+        print("daxil etdiyiniz reqem:{} {}".format(sesli_reqem_onluq[0], sesli_reqem_adi[8]))
+    elif reqem_daxil == 20:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[1]))
+    elif reqem_daxil == 21:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[0]))
+    elif reqem_daxil == 22:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[1]))
+    elif reqem_daxil == 23:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[2]))
+    elif reqem_daxil == 24:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[3]))
+    elif reqem_daxil == 25:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[4]))
+    elif reqem_daxil == 26:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[5]))
+    elif reqem_daxil == 27:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[6]))
+    elif reqem_daxil == 28:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[7]))
+    elif reqem_daxil == 29:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[1], sesli_reqem_adi[8]))
+    elif reqem_daxil == 30:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[2]))
+    elif reqem_daxil == 31:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[0]))
+    elif reqem_daxil == 32:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[1]))
+    elif reqem_daxil == 33:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[2]))
+    elif reqem_daxil == 34:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[3]))
+    elif reqem_daxil == 35:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[4]))
+    elif reqem_daxil == 36:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[5]))
+    elif reqem_daxil == 37:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[6]))
+    elif reqem_daxil == 38:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[7]))
+    elif reqem_daxil == 39:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[2], sesli_reqem_adi[8]))
+    elif reqem_daxil == 40:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[3]))
+    elif reqem_daxil == 41:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[0]))
+    elif reqem_daxil == 42:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[1]))
+    elif reqem_daxil == 43:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[2]))
+    elif reqem_daxil == 44:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[3]))
+    elif reqem_daxil == 45:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[4]))
+    elif reqem_daxil == 46:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[5]))
+    elif reqem_daxil == 47:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[6]))
+    elif reqem_daxil == 48:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[7]))
+    elif reqem_daxil == 49:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[3], sesli_reqem_adi[8]))
+    elif reqem_daxil == 50:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[4]))
+    elif reqem_daxil == 51:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[0]))
+    elif reqem_daxil == 52:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[1]))
+    elif reqem_daxil == 53:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[2]))
+    elif reqem_daxil == 54:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[3]))
+    elif reqem_daxil == 55:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[4]))
+    elif reqem_daxil == 56:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[5]))
+    elif reqem_daxil == 57:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[6]))
+    elif reqem_daxil == 58:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[7]))
+    elif reqem_daxil == 59:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[4], sesli_reqem_adi[8]))
+    elif reqem_daxil == 60:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[5]))
+    elif reqem_daxil == 61:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[0]))
+    elif reqem_daxil == 62:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[1]))
+    elif reqem_daxil == 63:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[2]))
+    elif reqem_daxil == 64:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[3]))
+    elif reqem_daxil == 65:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[4]))
+    elif reqem_daxil == 66:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[5]))
+    elif reqem_daxil == 67:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[6]))
+    elif reqem_daxil == 68:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[7]))
+    elif reqem_daxil == 69:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[5], sesli_reqem_adi[8]))
+    elif reqem_daxil == 70:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[6]))
+    elif reqem_daxil == 71:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[0]))
+    elif reqem_daxil == 72:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[1]))
+    elif reqem_daxil == 73:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[2]))
+    elif reqem_daxil == 74:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[3]))
+    elif reqem_daxil == 75:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[4]))
+    elif reqem_daxil == 76:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[5]))
+    elif reqem_daxil == 77:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[6]))
+    elif reqem_daxil == 78:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[7]))
+    elif reqem_daxil == 79:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[6], sesli_reqem_adi[8]))
+    elif reqem_daxil == 80:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[7]))
+    elif reqem_daxil == 81:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[0]))
+    elif reqem_daxil == 82:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[1]))
+    elif reqem_daxil == 83:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[2]))
+    elif reqem_daxil == 84:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[3]))
+    elif reqem_daxil == 85:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[4]))
+    elif reqem_daxil == 86:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[5]))
+    elif reqem_daxil == 87:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[6]))
+    elif reqem_daxil == 88:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[7]))
+    elif reqem_daxil == 89:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[7], sesli_reqem_adi[8]))
+    elif reqem_daxil == 90:
+        print("daxil etdiyiniz reqem: {}".format(sesli_reqem_onluq[8]))
+    elif reqem_daxil == 91:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[0]))
+    elif reqem_daxil == 92:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[1]))
+    elif reqem_daxil == 93:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[2]))
+    elif reqem_daxil == 94:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[3]))
+    elif reqem_daxil == 95:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[4]))
+    elif reqem_daxil == 96:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[5]))
+    elif reqem_daxil == 97:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[6]))
+    elif reqem_daxil == 98:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[7]))
+    elif reqem_daxil == 99:
+        print("daxil etdiyiniz reqem: {} {}".format(sesli_reqem_onluq[8], sesli_reqem_adi[8]))
+    elif reqem_daxil == 100:
+        print("daxil etdiyiniz reqem: {}" .format(sesli_reqem_onluq[9]))
