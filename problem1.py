131db324fb1c69502d64cb1e6fa91b7b4a70a717
CeferAzmiu01 <47792097+CeferAzmiu01@users.noreply.github.com>
2019-02-19 22:46:38 +0400

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,7 @@
-
+#CeferMemmedzade  #1citapshiriq
+reqem = int(input("reqemi daxil edin: "))
+a  = reqem //1000
+b = (reqem - a*1000)//100
+c = (reqem - a*1000 - b*100)//10
+d =reqem - a*1000 - b*100 - c*10
+print("Reqemlerin toplami: ", a+b+c+d)
